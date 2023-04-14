5ea90022c7fda792ead43fe14a2ef1d64807e478
Pauline Davis <pauljdavi.t21@btinternet.com>
2023-04-14 14:53:41 +0100

    ifelse and booleans

---
+++ youtube/src/ComparisonOps.java
@@ -0,0 +1,27 @@
+public class ComparisonOps {
+
+    public static void main(String[] args) {
+       /** int a = 5;
+        int b = 4;**/
+        // == is equal to
+        //<= less than or equal to
+        //>= greater than or equal to
+        //< less than
+        //> greater than
+        // = assign a variable
+        //boolean c = (a >= b);
+
+        double a = 2.4;
+
+        if(a == 2.4) {
+            System.out.println("They are equal");
+        }
+        if(a > 7) {
+            System.out.println("Greater than 7");
+        }
+        if(true || false) {
+            System.out.println("a == 2.4 or a == 2.5");
+        }
+
+    }
+}
---
+++ youtube/src/ifElse.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ifElse {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a grade: ");
+        int n = scan.nextInt();
+//        System.out.println(n);
+
+        if(n >= 90 && n <= 100) {
+            System.out.println("A");
+        } else if (n >= 80 && n <= 90) {
+            System.out.println("C");
+        } else if (n >= 70 && n <= 80) {
+            System.out.println("D");
+        } else if (n >= 65 && n <= 70) {
+            System.out.println("E");
+        } else if (n >= 49 && n <= 65) {
+            System.out.println("F");
+        } else {
+            System.out.println("The number you entered is out of range");
+        }
+    }
+
+}
