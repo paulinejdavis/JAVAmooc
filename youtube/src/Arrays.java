a44c5748a7d6768d49929d3c0471046e4a7322a0
Pauline Davis <pauljdavi.t21@btinternet.com>
2023-04-16 12:55:08 +0100

    arrays and for loops

---
+++ youtube/src/Arrays.java
@@ -0,0 +1,23 @@
+public class Arrays {
+    //swapping flowers
+    public static void main(String[] args) {
+        System.out.println("Welcome to Zombieland!");
+
+        String backpack[] = {"Roses", "Daffodils", "Clementines"};
+        String zombies[] = {"Garden", "Meadow", "Balcony"};
+        int hatedZombiesNums[] = {4, 3, 2, 1};
+
+        System.out.println("Backpack items are");
+        System.out.println(backpack[0]);
+        System.out.println(backpack[1]);
+        System.out.println(backpack[2]);
+
+        System.out.println("These are all the Zombie locations:");
+        System.out.println(zombies[0]);
+        System.out.println(zombies[1]);
+        System.out.println(zombies[2]);
+
+        System.out.println(hatedZombiesNums[3]);
+
+    }
+}
---
+++ youtube/src/ForLoops.java
@@ -0,0 +1,17 @@
+public class ForLoops {
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 10; i++) {
+            System.out.println("I love dogs!");
+        }
+        for(int i=0; i < 15; i++){
+            System.out.println(i);
+        }
+        //print out all values in an arrray
+
+        int[] grades = {98,100, 101, 83};
+
+        for(int i= 0; i < gtades.length; i++){
+            System.out.println(i);
+    }
+}
