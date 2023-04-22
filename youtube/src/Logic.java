c65145af37c7d66b29617609a60e064e311d3ae3
Pauline Davis <pauljdavi.t21@btinternet.com>
2023-04-22 09:54:13 +0100

    add operators

---
+++ youtube/src/Logic.java
@@ -0,0 +1,19 @@
+public class Logic {
+
+    public static void main(String[] args) {
+
+        // || or
+        // && and
+        //! not
+
+        //boolean a = true || false;
+        //boolean a = true && false;
+        //boolean a = !true;
+        int a = 3;
+        int b = 4;
+        boolean c = !((a > b) && true);
+
+
+        System.out.println(c);
+    }
+}
