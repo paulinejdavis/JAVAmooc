5340da0af6387bc49b366b57644d5f1eeefe945c
Pauline Davis <pauljdavi.t21@btinternet.com>
2023-04-24 18:16:28 +0100

    declaration in java

--- youtube/src/Logic.java
+++ youtube/src/Logic.java
@@ -11,9 +11,14 @@ public class Logic {
         //boolean a = !true;
         int a = 3;
         int b = 4;
-        boolean c = !((a > b) && true);
+        //boolean c = !((a > b) && true);
+        boolean isSunny = false;
+        boolean amHappy = false;
 
+        if ((a < b && isSunny) || amHappy) {
+            System.out.println("it's gonna be a good day");
+        }
 
-        System.out.println(c);
+            //System.out.println(c);
     }
 }
---
+++ youtube/src/Pizza.java
@@ -0,0 +1,9 @@
+public class Pizza {
+    public static void main(String[] args) {
+        String pizza1 = "pineapple"; //instruction
+        String pizza2; //declaration
+        pizza2 = "bbq"; //assignment
+
+         }
+
+}
