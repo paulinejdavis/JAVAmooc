26f7912ee8151599ef6d3c3cbcecf630140eac60
Pauline Davis <pauljdavi.t21@btinternet.com>
2023-04-26 13:57:56 +0100

    breaking out of switch statement and for loop

---
+++ youtube/src/Break.java
@@ -0,0 +1,30 @@
+public class Break {
+    public static void main(String[] args) {
+//        int i = 0;
+//        while (i < 3) {
+//        System.out.println("Hi!!");
+//        i++;
+//        break;
+//        int[] numbers = {10,20,30,40,50};
+//        for (int i=0; i < numbers.length; i++){
+//            if (numbers[i] == 30){
+//                break;
+//                //exit
+//            }
+//            System.out.println(numbers[i]);
+        //switch statment
+        int i = 5;
+        switch(i) {
+            case 0:
+                System.out.println("Zero!");
+                break;
+            case 1:
+                System.out.println("One!");
+                break;
+            default:
+                System.out.println("WTH!!!");
+                break;
+        }
+
+    }
+}
