8ffdc9da75a5e6226660e605a497f79f4f35eed8
Pauline Davis <pauljdavi.t21@btinternet.com>
2023-04-27 10:22:37 +0100

    breaking out of nested loops and methods

--- youtube/src/Break.java
+++ youtube/src/Break.java
@@ -13,18 +13,28 @@ public class Break {
 //            }
 //            System.out.println(numbers[i]);
         //switch statment
-        int i = 5;
-        switch(i) {
-            case 0:
-                System.out.println("Zero!");
+//        int i = 5;
+//        switch(i) {
+//            case 0:
+//                System.out.println("Zero!");
+//                break;
+//            case 1:
+//                System.out.println("One!");
+//                break;
+//            default:
+//                System.out.println("WTH!!!");
+//                break;
+//        }
+        //nested for loop
+        for(int i = 0; i < 5; i++) {
+
+            for (int j = 0; j < 3; j++) {
+                System.out.println(i + "," + j);
                 break;
-            case 1:
-                System.out.println("One!");
-                break;
-            default:
-                System.out.println("WTH!!!");
-                break;
+            }
         }
+
+
 
     }
 }
